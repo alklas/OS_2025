9a0e8c916b44087b367d25853121ed7f36df3b8f
alexander <alklas@mail.ru>
2025-04-21 14:43:55 +0700

    Создали программу, выводящую аргументы командной строки

---
+++ pr006.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[], char* envp[]) {
+	printf("Количество аргументов, переданных программе: %d\n", argc);
+	printf("\n");
+	for (int i=0; i < argc; i++)
+		printf("Аргумент[%d]: %s\n", i, argv[i]);
+	printf("\n");
+	return 0;
+}
